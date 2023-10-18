abffd7fb7b72945ab243fc36fa9d173282d12886
Slava Nossar <slava.nossar@gmail.com>
2023-10-18 13:07:47 +1100

    add example

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,35 @@
 <template>
-  <div />
+  <div class="flex flex-col justify-center items-center space-y-4 h-screen">
+    <p>
+      useRafFn without fpsLimit
+      {{ hasRunWithoutFpsLimit ? 'has' : "hasn't" }} run (delta:
+      {{ lastDeltaWithoutFpsLimit }})
+    </p>
+    <p>
+      useRafFn with fpsLimit
+      {{ hasRunWithFpsLimit ? 'has' : "hasn't" }} run (delta:
+      {{ lastDeltaWithFpsLimit }})
+    </p>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const hasRunWithoutFpsLimit = ref(false)
+const lastDeltaWithoutFpsLimit = ref(0)
+
+const hasRunWithFpsLimit = ref(false)
+const lastDeltaWithFpsLimit = ref(0) // Will never change
+
+useRafFn(({ delta }) => {
+  lastDeltaWithoutFpsLimit.value = delta
+  hasRunWithoutFpsLimit.value = true
+})
+
+useRafFn(
+  ({ delta }) => {
+    lastDeltaWithFpsLimit.value = delta
+    hasRunWithFpsLimit.value = true
+  },
+  { fpsLimit: 60 },
+)
+</script>
